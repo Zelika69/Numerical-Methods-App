88d9d477636f0f3968881c660fc7f1814c26abbd
Zelika69 <benjamoraleshdz@gmail.com>
2025-07-13 16:08:33 -0600

    prt1

--- MetodosNumericosApp/Form1.Designer.cs
+++ MetodosNumericosApp/Form1.Designer.cs
@@ -30,14 +30,21 @@
         {
             this.textA = new System.Windows.Forms.TextBox();
             this.textB = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtFuncion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblFuncion = new System.Windows.Forms.Label();
+            this.rtbResultados = new System.Windows.Forms.RichTextBox();
+            this.cmbTolerancia = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textA
             // 
-            this.textA.Location = new System.Drawing.Point(271, 63);
+            this.textA.Location = new System.Drawing.Point(114, 83);
             this.textA.MaxLength = 100;
             this.textA.Name = "textA";
             this.textA.Size = new System.Drawing.Size(100, 20);
@@ -45,50 +52,118 @@
             // 
             // textB
             // 
-            this.textB.Location = new System.Drawing.Point(271, 117);
+            this.textB.Location = new System.Drawing.Point(114, 152);
             this.textB.MaxLength = 100;
             this.textB.Name = "textB";
             this.textB.Size = new System.Drawing.Size(100, 20);
             this.textB.TabIndex = 1;
             // 
-            // label1
+            // lblA
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(134, 69);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(41, 90);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(44, 13);
+            this.lblA.TabIndex = 2;
+            this.lblA.Text = "Valor A:";
+            this.lblA.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // lblB
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(134, 124);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(41, 159);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(44, 13);
+            this.lblB.TabIndex = 3;
+            this.lblB.Text = "Valor B:";
             // 
-            // comboBox1
+            // btnCalcular
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(271, 194);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 5;
+            this.btnCalcular.Location = new System.Drawing.Point(72, 255);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // txtFuncion
+            // 
+            this.txtFuncion.Location = new System.Drawing.Point(83, 26);
+            this.txtFuncion.Name = "txtFuncion";
+            this.txtFuncion.Size = new System.Drawing.Size(100, 20);
+            this.txtFuncion.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tolerancia:";
+            // 
+            // lblFuncion
+            // 
+            this.lblFuncion.AutoSize = true;
+            this.lblFuncion.Location = new System.Drawing.Point(6, 29);
+            this.lblFuncion.Name = "lblFuncion";
+            this.lblFuncion.Size = new System.Drawing.Size(51, 13);
+            this.lblFuncion.TabIndex = 9;
+            this.lblFuncion.Text = "Función: ";
+            // 
+            // rtbResultados
+            // 
+            this.rtbResultados.Dock = System.Windows.Forms.DockStyle.Right;
+            this.rtbResultados.Location = new System.Drawing.Point(288, 0);
+            this.rtbResultados.Name = "rtbResultados";
+            this.rtbResultados.Size = new System.Drawing.Size(332, 305);
+            this.rtbResultados.TabIndex = 10;
+            this.rtbResultados.Text = "";
+            // 
+            // cmbTolerancia
+            // 
+            this.cmbTolerancia.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTolerancia.FormattingEnabled = true;
+            this.cmbTolerancia.Items.AddRange(new object[] {
+            "0.1",
+            "0.01",
+            "0.001",
+            "0.0001"});
+            this.cmbTolerancia.Location = new System.Drawing.Point(114, 202);
+            this.cmbTolerancia.Name = "cmbTolerancia";
+            this.cmbTolerancia.Size = new System.Drawing.Size(100, 21);
+            this.cmbTolerancia.TabIndex = 11;
+            this.cmbTolerancia.SelectedIndexChanged += new System.EventHandler(this.cmbTolerancia_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblFuncion);
+            this.groupBox1.Controls.Add(this.txtFuncion);
+            this.groupBox1.Location = new System.Drawing.Point(257, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(201, 281);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parámetros";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 305);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(620, 305);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbTolerancia);
+            this.Controls.Add(this.rtbResultados);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
             this.Controls.Add(this.textB);
             this.Controls.Add(this.textA);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,9 +173,15 @@
 
         private System.Windows.Forms.TextBox textA;
         private System.Windows.Forms.TextBox textB;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtFuncion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblFuncion;
+        private System.Windows.Forms.RichTextBox rtbResultados;
+        private System.Windows.Forms.ComboBox cmbTolerancia;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- MetodosNumericosApp/Form1.cs
+++ MetodosNumericosApp/Form1.cs
@@ -16,5 +16,15 @@ namespace MetodosNumericosApp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbTolerancia_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
